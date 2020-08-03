28146bf2110d58bfe95cc5f829d1811f4652505b
Wahhab1801 <abdul.wahhab@khi.iba.edu.pk>
2020-08-03 01:45:54 -0500

    Heroku URL added for Socket.io client

--- public/client.js
+++ public/client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000'); //location of where server is hosting socket app
+const socket = io(`http://lets-chat1.herokuapp.com`); //location of where server is hosting socket app
 
 socket.on('chat-message', data => {
     console.log(data)
