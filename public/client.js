e4546e13a13f376095ed5efd895778c2581958f1
Wahhab1801 <abdul.wahhab@khi.iba.edu.pk>
2020-08-03 01:10:01 -0500

    Uploading to Github

---
+++ public/client.js
@@ -0,0 +1,37 @@
+const socket = io('http://localhost:3000'); //location of where server is hosting socket app
+
+socket.on('chat-message', data => {
+    console.log(data)
+});
+
+// query DOM
+const message = document.getElementById('message');
+handle = document.getElementById('handle');
+button = document.getElementById('submit');
+output = document.getElementById('output');
+typing = document.getElementById('typing');
+
+//  send typing message
+message.addEventListener('keypress', () => {
+    socket.emit('userTyping', hanlde.value)
+})
+// Emit events
+
+
+button.addEventListener('click', () => {
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    })
+    document.getElementById('message').value = "";
+})
+
+// Listen to events
+
+socket.on('chat', (data) => {
+    //typing.innerHTML="";
+    output.innerHTML += '<p> <strong>' + data.handle + ': </strong>' + data.message + '</p>'
+})
+socket.on('userTyping', (data) => {
+    typing.innerHTML += '<p> <em>' + data + ' is typing... </em></p>'
+})
